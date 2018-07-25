8f845dad561941824eea6313571ef4a115332b2c
Sarita Panchang <panchang@mail.usf.edu>
2018-07-25 09:57:11 -0400

    configuration

--- worksheet.sh
+++ worksheet.sh
@@ -1,10 +1,10 @@
 # configure git
 
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name "Sarita Panchang"
+git config --global user.email "panchang@mail.usf.edu"
 git commit --no-edit --amend --reset-author
 
 # link your local repository to the origin repository on GitHub
 
-git remote add origin ...
-git push -u origin master
+#git remote add origin ...
+#git push -u origin master
